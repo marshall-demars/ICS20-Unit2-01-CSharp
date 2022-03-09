5c247d6c1bc8a62d7900e851dc86c901d03f09fb
marshall-demars <99354910+marshall-demars@users.noreply.github.com>
2022-03-09 18:19:47 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,14 @@
+// Created by: Marshall
+//
+// This program displays, "Hello, World!"
+
+using System;
+
+class Program
+{
+    public static void Main (string[] args)
+    {
+        Console.WriteLine ("Hello, World!");
+        Console.WriteLine ("\nDone.");
+    }
+}
